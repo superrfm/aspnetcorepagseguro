e88dbe5c3b93d7cca359fe26d05446e554c04a2d
Rafael Magalhães <superrfm@hotmail.com>
2019-04-23 19:09:14 -0300

    Add project files.

---
+++ pagueseguro.aspnetcore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pagueseguro.aspnetcore.Models;
+using Uol.PagSeguro.Domain;
+using Uol.PagSeguro.Resources;
+
+namespace pagueseguro.aspnetcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult ObterConfiguracoes() {
+
+            AccountCredentials credentials = PagSeguroConfiguration.Credentials(false);
+            return Ok(credentials);
+
+        }
+    }
+}
---
+++ pagueseguro.aspnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pagueseguro.aspnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
